d21ba275cce97fa99a317f3b9989f1f11e4f084b
Tainpoul <Tainpoul@DESKTOP-68DG3JK>
2021-08-30 15:08:54 +0700

    Add project files.

---
+++ lab5.1/Source.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<conio.h>
+int x, y;
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void erase_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf("       ");
+}
+void draw_ship(int x, int y)
+{
+	gotoxy(x, y);
+	printf(" <-0-> ");
+}
+int main()
+{
+	char ch = ' ';
+	int x = 38, y = 20;
+	draw_ship(x, y);
+	do {
+		if (_kbhit()) {
+			ch = _getch();
+			if (ch == 'a' && x > 0)
+			{
+				erase_ship(x, y);
+				draw_ship(--x, y);
+			}
+			if (ch == 'd' && x < 80)
+			{
+				erase_ship(x, y);
+				draw_ship(++x, y);
+			}
+			if (ch == 'w' && y > 0)
+			{
+				erase_ship(x, y);
+				draw_ship(x, --y);
+			}
+			if (ch == 's' && y < 23)
+			{
+				erase_ship(x, y);
+				draw_ship(x, ++y);
+			}
+			fflush(stdin);
+		}
+		Sleep(100);
+	} while (ch != 'x');
+	return 0;
+}
